abb2737ecad99422440868083f57c6bc4c1c7b9b
unknown <giedrebielske@hmail.com>
2022-08-15 14:02:35 +0300

    Added few changes in style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
   const [showGreeting, setShowGreeting] = useState(false);
   return (
     <div>
+      <h1>Greeting app</h1>
       <input type="text"
         placeholder="What's your name?"
         onChange={(e) => [setName(e.target.value)]}
